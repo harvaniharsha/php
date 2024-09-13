d262fe70a6e73e422bf82b9fc0b8478749414879
harvaniharsha <harvaniharsha@gmail.com>
2024-09-13 11:15:20 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>
+</title>
+</head>
+<body bgcolor="pink">
+<form action="dash.php" method="GET">
+<table border="1" cellspadding="0" height="100" width="100" align="center" bgcolor="green">
+   <tr><td>enter about me<input type="username" name="about"><br></td></tr>
+   <tr><td>enter email<input type="email" name="em"><br></td></tr>
+   <tr><td>enter projectdetail<input type="class"name="cl"><br></td></tr>
+   <tr><td>enter task1<input type="php" name="ph"><br></td></tr>
+   <tr><td><button align="center"><input type="submit"></button><br></td></tr>
+  </table>
+</body>
+</form>
+</html>
